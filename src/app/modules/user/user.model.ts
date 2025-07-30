3371c5a0ec21be033d6c12205b0f89f09fbbe91f
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-30 20:12:06 +0600

    feat: user model route and whole route

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,48 @@
+import { model, Schema } from "mongoose";
+import { IAuthProvider, isActive, IUser, Role } from "./user.interface";
+
+
+const authProvierSchema = new Schema<IAuthProvider>({
+
+    provider: { type: String, required: true },
+    providerId: { type: String, required: true }
+
+},
+{
+    versionKey : false,
+    _id: false 
+}
+    ,)
+
+
+const userSchema = new Schema<IUser>({
+    name  :{type : String, required : true},
+    email : {type : String, required : true, unique : true},
+    password : {type : String},
+    role :{
+        type : String,
+        enum : Object.values(Role),
+        default : Role.USER
+    },
+    phone : {type : String},
+    picture : {type : String},
+    address : {type : String},
+    isDeleted : {type : Boolean, default : false},
+    isActive : {
+        type : String,
+        enum : Object.values(isActive),
+        default : isActive.ACTIVE
+    },
+    isVarified : {type : Boolean, default : false},
+    auths :[authProvierSchema],
+
+
+},
+    
+    {
+    timestamps : true,
+    versionKey : false
+})
+
+
+export const User = model<IUser>('User', userSchema);
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
+import { userRoutes } from "../modules/user/user.routes";
 
 export const router = Router();
 
 
 const moduleRoutes =[
    
-   
+   {
+    path: '/users',
+    route: userRoutes
+   }
    
    
 ]
