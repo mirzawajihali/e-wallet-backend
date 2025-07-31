f3bdb73e042216dd043988a05ef501f59704ff26
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-31 21:03:04 +0600

    feat : send money and withdrawal

--- src/app/modules/wallet/wallet.services.ts
+++ src/app/modules/wallet/wallet.services.ts
@@ -4,6 +4,7 @@ import  httpStatus from "http-status-codes";
 import { Wallet } from "./wallet.model";
 import { TransactionStatus, TransactionType } from "../transaction/transaction.interface";
 import { Transaction } from "../transaction/transaction.model";
+import { User } from "../user/user.model";
 class WalletService {
     async getMyWallet(userId : string){
         const wallet = await Wallet.findOne({userId}).populate('userId', 'name email role');
@@ -52,5 +53,106 @@ class WalletService {
 
        }
 
+
+       async withdrawMoney(userId : string, amount: number){
+        const session = await mongoose.startSession();
+       try{
+        session.startTransaction();
+         const wallet  = await Wallet.findOne({userId}).session(session);
+         if(!wallet){
+            throw new AppError(httpStatus.NOT_FOUND, "Wallet not found");
+         }
+
+         if(wallet.balance < amount){
+            throw new AppError(httpStatus.BAD_REQUEST, "insufficient balance")
+         }
+
+         wallet.balance -= amount;
+
+         await wallet.save({session});
+
+        await Transaction.create([{
+                type: TransactionType.WITHDRAW,
+                amount,
+                fromWallet: wallet._id,
+                initiatedBy: userId,
+                status: TransactionStatus.COMPLETED,
+                description: 'Money withdrawn from wallet'
+            }], { session })
+
+
+            await session.commitTransaction();
+             console.log(`✅ Money withdrawn: ${amount} from user ${userId}`);
+            return wallet;
+        }
+        catch(error){
+            await session.abortTransaction();
+            console.error('❌ Failed to withdraw money from wallet:', error);
+            throw error;
+        }
+
+        finally {
+            session.endSession();
+        }
+
+
+
+    
      
+}
+
+        async sendMoney(fromUserId : string, toUserId: string, amount : number){
+        const session = await mongoose.startSession();
+        try{;
+
+            session.startTransaction();
+            // now have to find receiver by id
+
+            const toUser = await User.findOne({toUserId}).session(session);
+            if(!toUser){
+                throw new AppError(httpStatus.NOT_FOUND, "Receiver not found");
+            }
+
+            const fromWallet = await Wallet.findOne({userId: fromUserId}).session(session);
+            const toWallet = await Wallet.findOne({userId: toUserId}).session(session);
+            if(!fromWallet || !toWallet){
+                throw new AppError(httpStatus.NOT_FOUND, "Wallet not found");
+            }
+
+           if (fromWallet.balance < amount) {
+                throw new AppError(httpStatus.BAD_REQUEST, 'Insufficient balance');
+            }
+            
+            // Update balances
+            fromWallet.balance -= amount;
+            toWallet.balance += amount;
+            
+            await fromWallet.save({ session });
+            await toWallet.save({ session });
+
+            await Transaction.create([{
+                type: TransactionType.SEND_MONEY,
+                amount,
+                fromWallet: fromWallet._id,
+                toWallet: toWallet._id,
+                initiatedBy: fromUserId,
+                status: TransactionStatus.COMPLETED,
+                description: `Money sent to ${toUser.email}`
+            }], { session });
+
+            await session.commitTransaction();
+            console.log(`✅ Money sent: ${amount} from user ${fromUserId} to user ${toUserId}`);
+            return { fromWallet, toWallet };
+        }
+        catch(error){
+            await session.abortTransaction();
+            console.error('❌ Failed to send money:', error);
+            throw error;
+        }
+        finally {
+            session.endSession();
+        }
+        }
+
+
 }
