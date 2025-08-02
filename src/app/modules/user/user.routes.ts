2863bd3a7cb7f520916050ede3404d3c28ec9fd4
mirzawajihali <mirzawajihali00@gmail.com>
2025-08-02 20:57:31 +0600

    all users authorized

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { userController } from "./user.controller";
 import { ValidateRequest } from "../../middlewares/ValidateRequest";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 const router = Router();
 
 router.post("/register", ValidateRequest(createUserZodSchema), userController.createUser);
-
-router.get("/", userController.getAllUsers);
+router.patch("/:id", ValidateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), userController.updateUser);
+router.get("/",checkAuth("ADMIN"), userController.getAllUsers);
 
 
 
